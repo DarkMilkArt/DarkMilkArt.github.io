28b7f982268d2cc2ca61b2e6e10af6115d57e43b
Mila Kulikova <dark.milk.art@gmail.com>
2022-12-23 12:07:41 +0300

    left modal, and all sliders are done

--- scripts/swiper/swiper.js
+++ scripts/swiper/swiper.js
@@ -3,31 +3,61 @@
      const width = window.innerWidth;
     if (width < 550){        
         
-        const slider = document.querySelector('.brends-list__swiper');
-        const swiper = new Swiper(slider, {
-        // Optional parameters
+        const sliders = document.querySelectorAll('.swiper-container');
+        const sliderBrends = sliders[0];
+        const sliderDevices = sliders[1];
+        const sliderServices = sliders[2];
+               
+    const slider = {
         direction: 'horizontal',    
         spaceBetween: 16,
         slidesPerView: 2,
         centeredSlides: true,
         slidesOffsetBefore: -110,
     
-        // If we need pagination
+        
         pagination: {
         el: '.swiper-pagination',  
         type:'bullets',      
         },
     
-        // Navigation arrows
+        
         navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',        
         },          
-    });
+    }; 
+    const swiperBrends = new Swiper(sliderBrends, slider);
+    const swiperDevices = new Swiper(sliderDevices, slider);
+    const swiperServices = new Swiper(sliderServices, slider);
 } else {
 swiper.classList.remove('swiper-container');
 pagination.classList.add('hidden');
 }}
+)
+
+
+const burgerButton = document.querySelector('.nav-buttons__menu');
+burgerButton.addEventListener('click',function(){
+    const menu = document.querySelector('.main-menu');
+    const asideBar = menu.querySelector('.aside-bar');
+    const clicking = function () {
+        menu.classList.add('main-menu--show');
+        asideBar.classList.add('aside-bar--show');
+    }
+return clicking();
+}
+)
+const hideMenu = document.querySelector('.top__close')
+hideMenu.addEventListener('click',function(){
+    const menu = document.querySelector('.main-menu');
+    const asideBar = menu.querySelector('.aside-bar');
+    const clickingClose = function () {          
+        asideBar.classList.remove('aside-bar--show');
+        menu.classList.remove('main-menu--show');       
+    }
+return clickingClose();
+}
 )
 
 
